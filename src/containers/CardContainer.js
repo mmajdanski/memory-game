19db8cf4d8323f169cc9b5447295baa7bacc60f7
mmajdanski <michael.majdanski@gmail.com>
2018-09-06 10:22:39 -0700

    CardContainer Resolved

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -1,41 +1,18 @@
-<<<<<<< HEAD
 import React, {Component} from "react";
-=======
-import React, { Component } from "react";
 import PropTypes from "prop-types";
->>>>>>> 6557a1f9f2d001d53bcaf17d1ddc096d958e3edd
 
 import Card from "./../components/Card";
 import CardPropType from "./../propTypes/CardPropType";
 
 class CardContainer extends Component {
-<<<<<<< HEAD
-  state = {
-    flipped: false
-  };
-
-  flip = () => {
-    this.setState(prevState => {
-      return {
-        flipped: !prevState.flipped
-      };
-    });
-  };
-
-=======
->>>>>>> 6557a1f9f2d001d53bcaf17d1ddc096d958e3edd
   handleClick = () => {
-    this.props.onClick();
+    this
+      .props
+      .onClick();
   };
 
   render() {
-<<<<<<< HEAD
-    return <Card flipped={this.state.flipped} onClick={this.handleClick}/>;
-=======
-    return (
-      <Card flipped={this.props.card.flipped} onClick={this.handleClick} />
-    );
->>>>>>> 6557a1f9f2d001d53bcaf17d1ddc096d958e3edd
+    return (<Card flipped={this.props.card.flipped} onClick={this.handleClick}/>);
   }
 }
 
